c92db9a5edc0b04aeffc8a2b33d189b37ea13796
MeghaYewalekar <myoct002@gmail.com>
2025-08-05 17:54:16 +0530

    Project name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
     <header className="bg-white">
       <nav className="max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center ">
         {/* Logo */}
-        <a href="#" className="text-3xl font-bold">
-          <span className="text-orange-500">My</span>DailyFresh
+        <a href="#" className="text-3xl font-bold"><span className="text-orange-500">M</span>
+          Daily<span className="text-orange-500">Fresh</span>
         </a>
         {/* Desktop Menu */}
         <ul className="flex  gap-x-15">
