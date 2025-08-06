7de3cc2bda0369bd7539d45375a745c4dc0f17c5
MeghaYewalekar <myoct002@gmail.com>
2025-08-06 23:34:15 +0530

    Made Resposive Navbar and Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <section>
-      <div className="max-w-[1400px] mx-auto px-10 flex items-center">
+      <div className="max-w-[1400px] min-h-screen mx-auto px-10 flex flex-col md:flex-row items-center pt-35 md:pt-25">
         {/* Left Side Content  */}
         <div className="flex-1">
           <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">Export Best Quality...</span>
-          <h1 className="text-7xl/19 font-bold mt-4">Tasty Organic <span className =" text-orange-500">Fruits</span> & <span className =" text-orange-500">Veggies</span> In Your City</h1>
-          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-5">
+          <h1 className=" text-5xl/14 md:text-7xl/19 font-bold mt-4">Tasty Organic <span className =" text-orange-500">Fruits</span> & <span className =" text-orange-500">Veggies</span> In Your City</h1>
+          <p className="text-zinc-600 text-md md:text-lg max-w-[530px] mt-5 mb-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sunt eius neque itaque sit 
           </p>
           <Button content="Shop Now"/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoHeartFill } from "react-icons/go";
 import { HiMiniShoppingBag } from "react-icons/hi2";
 import { IoMdSearch } from "react-icons/io";
+import { TbMenu2, TbMenu3 } from "react-icons/tb";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <header className="bg-white">
-      <nav className="max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center ">
+    <header className="bg-white fixed top-0 right-0 left-0 ">
+      <nav className="max-w-[1400px] mx-auto px-10 h-[12vh] md:h-[14vh] flex justify-between items-center ">
         {/* Logo */}
-        <a href="#" className="text-3xl font-bold"><span className="text-orange-500">M</span>
+        <a href="#" className="text-3xl font-bold">
+          <span className="text-orange-500">M</span>
           Daily<span className="text-orange-500">Fresh</span>
         </a>
         {/* Desktop Menu */}
-        <ul className="flex  gap-x-15">
+        <ul className="md:flex  gap-x-15 hidden">
           <li>
             <a
               href="#"
@@ -49,22 +57,92 @@ const Navbar = () => {
 
         {/* Nav Action */}
         <div className="flex items-center gap-x-5">
-            {/* Input field */}
-            <div className="flex p-1 border-2 border-orange-500 rounded-full">
-                <input type="text" name="text" id="text" placeholder="Search.." autoComplete="off" 
-                className=" px-3 focus:outline-none"/>
-                <button className="bg-gradient-to-b from-orange-400 to-orange-500 text-xl text-white w-10 h-10 flex justify-center  items-center rounded-full "><IoMdSearch /></button>
-            </div>
-          <a href=""  className="text-zinc-800 text-2xl">
+          {/* Input field */}
+          <div className="md:flex p-1 border-2 border-orange-500 rounded-full hidden">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search.."
+              autoComplete="off"
+              className=" px-3 focus:outline-none"
+            />
+            <button className="bg-gradient-to-b from-orange-400 to-orange-500 text-xl text-white w-10 h-10 flex justify-center  items-center rounded-full ">
+              <IoMdSearch />
+            </button>
+          </div>
+          <a href="" className="text-zinc-800 text-2xl">
             <GoHeartFill />
           </a>
           <a href="" className="text-zinc-800 text-2xl">
             <HiMiniShoppingBag />
           </a>
+
+          {/* Hamburger  */}
+          <a
+            href="#"
+            onClick={toggleMenu}
+            className="text-zinc-800 text-2xl md:hidden"
+          >
+            {showMenu ? <TbMenu3 /> : <TbMenu2 />}
+          </a>
         </div>
+
+        {/* Mobile Menu */}
+        <ul
+          className={`flex flex-col gap-y-12 bg-orange-500/15 backdrop-blur-xl items-center rounded-xl p-10  gap-x-15 md:hidden absolute top-30 -left-full tranform -translate-x-1/2 transition-all duration-500  ${
+            showMenu ? "left-1/2" : ""
+          } `}
+        >
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-orange-500"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </a>
+          </li>
+          <li className="flex p-1 border-2 border-orange-500 rounded-full md:hidden">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search.."
+              autoComplete="off"
+              className=" px-3 focus:outline-none"
+            />
+            <button className="bg-gradient-to-b from-orange-400 to-orange-500 text-xl text-white w-10 h-10 flex justify-center  items-center rounded-full ">
+              <IoMdSearch />
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
